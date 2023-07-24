ceeb7080fc3fdc37da17aa654b736c0a68188e7d
Shashank Keshava <Shashank_k@intuit.com>
2023-07-24 15:27:20 +0530

    feat: board component

---
+++ src/tictactoe.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Sqaure({ value }) {
+	function handleClick() {
+		console.log('Clicked!');
+	}
+	return (
+		<button className="square" onClick={handleClick}>
+			{value}
+		</button>
+	);
+}
+
+function Board() {
+	return (
+		<>
+			<div className="board-row">
+				<Sqaure value={1} />
+				<Sqaure value={2} />
+				<Sqaure value={3} />
+			</div>
+			<div className="board-row">
+				<Sqaure value={4} />
+				<Sqaure value={5} />
+				<Sqaure value={6} />
+			</div>
+			<div className="board-row">
+				<Sqaure value={7} />
+				<Sqaure value={8} />
+				<Sqaure value={9} />
+			</div>
+		</>
+	);
+}
+
+function TicTacToe() {
+	return <Board />;
+}
+
+export default TicTacToe;
