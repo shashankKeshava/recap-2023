a5c3d9fc07b3546d4626fb5764ef8c448d76c6cd
Shashank Keshava <shashank_k@intuit.com>
2023-08-02 23:42:33 +0530

    feat: calculate winner function added

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -11,17 +11,18 @@ function Square({ value, handleSquareClick }) {
 
 function Board() {
 	const [squares, setSquare] = useState(Array(9).fill(null));
-	const [isXNext, setXisNext] = useState(false);
+	const [isXNext, setXisNext] = useState(true);
+	// const [winner,setWinner]=useState(null);
 
 	function handleClick(i) {
 		const nextSquare = squares.slice();
-		if (squares[i]) {
+		if (calculateWinner(squares) || squares[i]) {
 			return;
 		}
 		if (isXNext) {
-			nextSquare[i] = 'O';
-		} else {
 			nextSquare[i] = 'X';
+		} else {
+			nextSquare[i] = 'O';
 		}
 
 		setXisNext(!isXNext);
@@ -29,8 +30,17 @@ function Board() {
 		console.log('index:', i);
 	}
 
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = 'Winner is:' + winner;
+	} else {
+		status = 'Next player: ' + (isXNext ? 'X' : 'O');
+	}
+
 	return (
 		<>
+			<div>{status}</div>
 			<div className="board-row">
 				<Square value={squares[0]} handleSquareClick={() => handleClick(0)} />
 				<Square value={squares[1]} handleSquareClick={() => handleClick(1)} />
@@ -52,6 +62,26 @@ function Board() {
 
 function TicTacToe() {
 	return <Board />;
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 export default TicTacToe;
