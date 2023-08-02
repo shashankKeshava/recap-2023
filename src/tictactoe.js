7a40061ba9ecf5f2bd5e77b281d568d4bb79c9e1
Shashank Keshava <shashank_k@intuit.com>
2023-08-02 23:12:39 +0530

    feat: lifting state up

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,36 +1,41 @@
 import React, { useState } from 'react';
-import "./tictactoe.css"
+import './tictactoe.css';
 
-function Sqaure() {
-	const [value, setValue] = useState(null);
-	function handleClick() {
-		setValue('X');
-		console.log('Clicked!');
-	}
+function Square({ value, handleSquareClick }) {
 	return (
-		<button className="square" onClick={handleClick}>
+		<button className="square" onClick={handleSquareClick}>
 			{value}
 		</button>
 	);
 }
 
 function Board() {
+	const [squares, setSquare] = useState(Array(9).fill(null));
+	// console.log(squares);
+
+	function handleClick(i) {
+		const nextSquare = squares.slice();
+		nextSquare[i] = 'X';
+		setSquare(nextSquare);
+		console.log('index:', i);
+	}
+
 	return (
 		<>
 			<div className="board-row">
-				<Sqaure value="1" />
-				<Sqaure value="2" />
-				<Sqaure value="3" />
+				<Square value={squares[0]} handleSquareClick={() => handleClick(0)} />
+				<Square value={squares[1]} handleSquareClick={() => handleClick(1)} />
+				<Square value={squares[2]} handleSquareClick={() => handleClick(2)} />
 			</div>
 			<div className="board-row">
-				<Sqaure value="4" />
-				<Sqaure value="5" />
-				<Sqaure value="6" />
+				<Square value={squares[3]} handleSquareClick={() => handleClick(3)} />
+				<Square value={squares[4]} handleSquareClick={() => handleClick(4)} />
+				<Square value={squares[5]} handleSquareClick={() => handleClick(5)} />
 			</div>
 			<div className="board-row">
-				<Sqaure value="7" />
-				<Sqaure value="8" />
-				<Sqaure value="9" />
+				<Square value={squares[6]} handleSquareClick={() => handleClick(6)} />
+				<Square value={squares[7]} handleSquareClick={() => handleClick(7)} />
+				<Square value={squares[8]} handleSquareClick={() => handleClick(8)} />
 			</div>
 		</>
 	);
